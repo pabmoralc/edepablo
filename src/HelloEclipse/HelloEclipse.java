498834c7b9f42367e29ee71d7c7f86966437d96b
Pablo Moret <pabmoralc@alu.edu.gva.es>
2025-12-03 12:18:34 +0100

    Primer commit

---
+++ src/HelloEclipse/HelloEclipse.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package HelloEclipse;
+
+/**
+ * 
+ */
+public class HelloEclipse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module HelloEclipse {
+}
